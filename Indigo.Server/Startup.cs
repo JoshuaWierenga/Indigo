20c1d5b1a968546609d432b816ffc94e3732df68
Joshua Wierenga <joshua.wierenga.student@gmail.com>
2017-09-21 21:34:32 +1000

    Created users controller
    Will be come login controller in the future

--- Indigo.Server/Startup.cs
+++ Indigo.Server/Startup.cs
@@ -43,7 +43,7 @@ namespace Indigo.Server
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Users}/{action=Index}/{id?}");
             });
         }
     }
