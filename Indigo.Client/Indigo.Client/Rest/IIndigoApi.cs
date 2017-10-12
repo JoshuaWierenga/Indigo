0ce6d531f70526a18baac60c566b2a14f3173d70
Joshua Wierenga <joshua.wierenga.student@gmail.com>
2017-10-12 16:03:10 +1100

    Adding conversation editing in client

---
+++ Indigo.Client/Indigo.Client/Rest/IIndigoApi.cs
@@ -0,0 +1,29 @@
+﻿using Indigo.Core.Models;
+using Refit;
+using System.Threading.Tasks;
+
+namespace Indigo.Client.Core.Rest
+{
+    public interface IIndigoApi
+    {
+		[Get("/users/{username}")]
+		Task<User> GetUserAsync([Header("authUsername")] string authUsername, [Header("authPasswordHash")] string authPasswordHash,
+			string username);
+
+		[Put("/conversations/{conversationid}")]
+		Task PutConversationAsync([Header("authUsername")] string authUsername, [Header("authPasswordHash")] string authPasswordHash,
+			int conversationid, [Body] Conversation conversation);
+
+		[Post("/conversations")]
+		Task<Conversation> PostConversationAsync([Header("authUsername")] string authUsername, [Header("authPasswordHash")] string authPasswordHash, 
+			[Body] Conversation conversation);
+
+		[Post("/conversations/{conversationid}/users")]
+		Task<UserConversation> PostUserConversationAsync([Header("authUsername")] string authUsername, [Header("authPasswordHash")] string authPasswordHash,
+			int conversationid, [Body] UserConversation userConversation);
+
+		[Delete("/conversations/{conversationid}/users/{userid}")]
+		Task DeleteUserConversationAsync([Header("authUsername")] string authUsername, [Header("authPasswordHash")] string authPasswordHash,
+			int conversationid, int userid);
+	}
+}
---
+++ Indigo.Client/Indigo.Client/ViewModels/ModifyConversationViewModel.cs
@@ -0,0 +1,107 @@
+﻿using Indigo.Core.Models;
+using MvvmHelpers;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Indigo.Client.Core.ViewModels
+{
+	class ModifyConversationViewModel : ConversationsViewModel
+	{
+		Conversation _conversation;
+		public Conversation Conversation
+		{
+			get => _conversation;
+			set => SetProperty(ref _conversation, value);
+		}
+
+		ObservableRangeCollection<UserConversation> _partners;
+		public ObservableRangeCollection<UserConversation> Partners
+		{
+			get => _partners;
+			set => SetProperty(ref _partners, value);
+		}
+
+		bool _newConversation;
+		public bool NewConversation
+		{
+			get => _newConversation;
+			set => SetProperty(ref _newConversation, value);
+		}
+
+		public ModifyConversationViewModel(User existingUser, Conversation existingConversation = null) : base(existingUser)
+		{
+			NewConversation = existingConversation == null;
+			Conversation = NewConversation ? new Conversation() : existingConversation;
+
+			Partners = new ObservableRangeCollection<UserConversation>();
+
+			if (NewConversation)
+			{
+				Partners.Add(new UserConversation
+				{
+					isAdmin = true
+				});
+			}
+			else
+			{
+				Partners.AddRange(Conversation.UserConversations);
+			}
+		}
+
+		//TODO handle group chats
+		public async Task SaveConversation()
+		{
+			if (NewConversation)
+			{
+				Conversation = await Server.CreateConversationAsync(User, Conversation);
+			}
+			else
+			{
+				await Server.PutConversationAsync(User, Conversation);
+			}
+
+			if (Conversation.UserConversations.Count() == 0)
+			{
+				//TODO tell user to enter usernames 
+				return;
+			}
+			else if (!Conversation.isGroupChat && Conversation.UserConversations.Count > 2)
+			{
+				//TOOD tell user that only one username can be entered for private chats
+				return;
+			}
+
+			/*for (int i = 0; i < partners.Length; i++)
+			{
+				//TODO handle group chats
+				bool userLevel = true;
+
+				await Server.CreateUserConversationAsync(User, Conversation, await GetPartnerUser(partners[i].Trim()), userLevel);
+			}*/
+		}
+
+		async Task<User> GetPartnerUser(string partnerUsername)
+		{
+			return await Server.GetUserAsync(User, partnerUsername);
+		}
+
+		public void AddPartner()
+		{
+			if (Conversation.isGroupChat || Partners.Count == 0)
+			{
+				Partners.Add(new UserConversation
+				{
+					isAdmin = false
+				});
+			}
+		}
+
+		public void ChangeChatType(bool newChatType)
+		{
+			Conversation temp = Conversation;
+			temp.isGroupChat = true;
+			Conversation = null;
+			Conversation = temp;
+		}
+	}
+}
