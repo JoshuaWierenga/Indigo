ce54c0733836abd022307ef2a3a51f4042fe6a71
Joshua Wierenga <joshua.wierenga.student@gmail.com>
2017-10-15 11:29:59 +1100

    Added page model

---
+++ Indigo.Core/Models/Page.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Indigo.Core
+{
+    class Page
+    {
+        int PageId { get; set; }
+        string Name { get; set; }
+        string Message { get; set; }
+        DateTime LastEdited { get; set; }
+    }
+}
