69e88f9e8148debde2581cb85cb2ecd32da5e8f4
Joshua Wierenga <joshua.wierenga.student@gmail.com>
2017-10-18 19:55:58 +1100

    finished page viewer

---
+++ Indigo.Client/Indigo.Client/Helpers/FullLocalDateTimeConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Indigo.Client.Helpers
+{
+    public class FullLocalDateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime dateTime = (DateTime)value;
+            return dateTime.ToLocalTime().ToString("F");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime dateTime = DateTime.Parse((string)value);
+            return dateTime.ToUniversalTime();
+        }
+    }
+}
