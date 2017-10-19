19557c667475ce3fc35c08068879e4f2da342a8a
Joshua Wierenga <joshua.wierenga.student@gmail.com>
2017-10-19 15:28:50 +1100

    Adding markdown support

---
+++ Indigo.Server/TagHelpers/MarkdownTagHelper.cs
@@ -0,0 +1,34 @@
+﻿using HeyRed.MarkdownSharp;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Indigo.Server.TagHelpers
+{
+    [HtmlTargetElement("markdown")]
+    public class MarkdownTagHelper : TagHelper
+    {
+        [HtmlAttributeName("text")]
+        public string Text { get; set; }
+
+        [HtmlAttributeName("source")]
+        public ModelExpression Source { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var markdownTransformer = new Markdown();
+            if (Source != null)
+            {
+                Text = Source.Model.ToString();
+            }
+
+            var result = markdownTransformer.Transform(Text);
+            output.TagName = "div";
+            output.Content.SetHtmlContent(result);
+            output.TagMode = TagMode.StartTagAndEndTag;
+        }
+    }
+}
